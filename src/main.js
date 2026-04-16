fc6002e1f7690be05e5b0c5f5939b2e10dc065dd
Fabio Capello <souza.fabiocapello@gmail.com>
2026-04-15 22:46:16 -0300

    Faz o dropdown do nav funcionar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const dropdownBtn = document.querySelector('#dropdown-menu');
+const navList = document.querySelector('#nav-list');
+const nav = document.querySelector('nav');
+
+// Adiciona interatividade para o botao do nav
+dropdownBtn.addEventListener('click', () => {
+  if (navList.classList.contains('hidden')) {
+    navList.classList.remove('hidden');
+    navList.classList.add('flex');
+    nav.classList.remove('justify-between');
+  } else if (navList.classList.contains('flex')) {
+    navList.classList.add('hidden');
+    navList.classList.remove('flex');
+    nav.classList.add('justify-between');
+  }
+})
