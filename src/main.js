c09f63cfa3e9f781f9ae3b84870c393f00bae12e
Fabio Capello <souza.fabiocapello@gmail.com>
2026-04-19 18:16:46 -0300

    feat: botao da calculadora

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const navList = document.querySelector('#nav-list');
 const nav = document.querySelector('nav');
 
 const monsterDataBtn = document.querySelector('#include-monster-data');
+const executeCalculationBtn = document.querySelector('#execute-calculation');
 
 // Adiciona interatividade para o botao do nav
 dropdownBtn.addEventListener('click', () => {
@@ -19,18 +20,18 @@ dropdownBtn.addEventListener('click', () => {
   }
 })
 
-// Adiciona interatividade no SVG do botao de execucao da calculadora
+// Adiciona interatividade no icone do botao de execucao da calculadora
 monsterDataBtn.addEventListener('click', () => {
   if (monsterDataBtn.classList.contains('toggle-on')) {
     // Toggle Value
     monsterDataBtn.classList.remove('toggle-on');
     monsterDataBtn.classList.add('toggle-off')
 
-    // Hides toggle-on SVG
+    // Esconde icone do toggle on
     monsterDataBtn.children[1].classList.remove('block');
     monsterDataBtn.children[1].classList.add('hidden');
 
-    // Displays toggle-off SVG
+    // Mostra icone do toggle off
     monsterDataBtn.children[2].classList.add('block');
     monsterDataBtn.children[2].classList.remove('hidden');
   } else if (monsterDataBtn.classList.contains('toggle-off')) {
@@ -38,12 +39,31 @@ monsterDataBtn.addEventListener('click', () => {
     monsterDataBtn.classList.remove('toggle-off');
     monsterDataBtn.classList.add('toggle-on')
 
-    // Displays toggle-on SVG
+    // Mostra icone do toggle on
     monsterDataBtn.children[1].classList.remove('hidden');
     monsterDataBtn.children[1].classList.add('block');
 
-    // Hides toggle-off SVG
+    // Esconde icone do toggle off
     monsterDataBtn.children[2].classList.add('hidden');
     monsterDataBtn.children[2].classList.remove('block');
   }
 })
+
+function tokenCalculation() {
+  // Pega os valores da calculadora
+  const income = document.querySelector('#income-tpm').value;
+  const killBonus = document.querySelector('#kill-bonus').value;
+  const killsPerMinute = document.querySelector('#kpm').value;
+
+  // Calcula o income
+  const incomeKills = killBonus * killsPerMinute;
+  const incomeHour = (income * 60) + (incomeKills * 60);
+  const incomeDay = incomeHour * 24 + (incomeKills * 60 * 24);
+  const incomeWeek = incomeDay * 7 + (incomeKills * 60 * 24 * 7);
+
+  // Imprime o income na tela do usuario
+  alert(`Hourly Income: ${incomeHour}\nDaily Income: ${incomeDay}\nWeekly Income: ${incomeWeek}`)
+  return incomeHour, incomeDay, incomeWeek;
+}
+
+executeCalculationBtn.addEventListener('click', () => {tokenCalculation();});
